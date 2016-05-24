bc4335cfb1328b3630934584d158b752263c8157
Noe Medina <noemi.medina@intelygenz.com>
2016-05-24 15:56:24 +0200

    Fix results for bad request

--- js/acrosstheuniverse.js
+++ js/acrosstheuniverse.js
@@ -2,32 +2,11 @@ var apiKey = 'HwoYeicxD7xs6igwFnRxASPVjqtwhOWf4VqcD1pO';
 
 var randomNumber = Math.floor(Math.random() * (1500 - 300 + 1)) + 300; // Para usar la API llamándola con el número de sol
 
-// Calculando la fecha de hoy
-
-// var yesterday = new Date();
-// yesterday.setDate(yesterday.getDate()-1);
-// var dd = yesterday.getDate();
-// var mm = yesterday.getMonth()+1;
-// var yyyy = yesterday.getFullYear();
-//
-// if(dd<10) {
-//     dd='0'+dd
-// }
-//
-// if(mm<10) {
-//     mm='0'+mm
-// }
-//
-// var currentDate = yyyy+'-'+mm+'-'+dd;
-
 
 // Arrancando
 nasaRequest("https://api.nasa.gov/planetary/apod?api_key=" + apiKey, "planetary");
 
-// nasaRequest("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + randomNumber +  "&api_key=" + apiKey, "marsRovers");
-
-nasaRequest("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + randomNumber +  "&api_key=" + apiKey, "marsRovers"); // así no funciona
-//nasaRequest("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-5-9&api_key=" + apiKey, "marsRovers"); // así sí funciona
+nasaRequest("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + randomNumber +  "&api_key=" + apiKey, "marsRovers");
 
 
 function pushToHTML(datos,container) {
@@ -50,18 +29,16 @@ function pushToHTML(datos,container) {
         document.getElementById('nasa-photo-description').innerHTML = datos.explanation;
     }
     if(container == 'marsRovers'){
-        if (datos.errors === "No Photos Found"){
-            photoMars.classList.add("hidden");
-        } else {
-            noPhotoMars.classList.add("hidden");
-            document.getElementById('mars-photo').setAttribute("src", datos.photos[0].img_src);
-            document.getElementById('mars-photo').setAttribute("atl", datos.photos[0].full_name);
-            document.getElementById('rover-name').innerHTML = datos.photos[0].rover.name;
-            document.getElementById('sol-date').innerHTML = datos.photos[0].sol;
-            document.getElementById('earth-date').innerHTML = datos.photos[0].earth_date;
-            document.getElementById('landing-date').innerHTML = datos.photos[0].rover.landing_date;
-            document.getElementById('no-photos').innerHTML = datos.error;
-        }
+        noPhotoMars.classList.add("hidden");
+        photoMars.classList.remove("hidden");
+        photoMars.classList.add("show");
+        document.getElementById('mars-photo').setAttribute("src", datos.photos[0].img_src);
+        document.getElementById('mars-photo').setAttribute("atl", datos.photos[0].full_name);
+        document.getElementById('rover-name').innerHTML = datos.photos[0].rover.name;
+        document.getElementById('sol-date').innerHTML = datos.photos[0].sol;
+        document.getElementById('earth-date').innerHTML = datos.photos[0].earth_date;
+        document.getElementById('landing-date').innerHTML = datos.photos[0].rover.landing_date;
+        document.getElementById('no-photos').innerHTML = datos.error;
     }
 }
 
